3a70cfa524b75d0e11e24a83ffd0cca274694d92
niklasben <nachtmahr2010@googlemail.com>
2017-06-12 22:49:01 +0200

    Changed

--- heimwerker/splittwothree.py
+++ heimwerker/splittwothree.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*
 
-import pprint
 import re
 
 
-def countTwo():
-    counterTwo = 0
-    with open('bc.processed2.csv', 'r') as openfiletwo:
-        for line in openfiletwo:
-            counterTwo += 1
+def countLength():
+    """Count Length of File."""
+    counter = 0
 
-    print counterTwo
+    with open('bc.processed3.csv', 'r') as openfile:
+        for line in openfile:
+            counter += 1
+            if counter == 1:
+                print line
+
+        print('Length: ', counter)
 
 
 def findSubcorpora():
@@ -79,65 +82,69 @@ def findDocumentsThree():
         print('lines divided by 2: ' + str(lines2))
 
 
-findDocumentsTwo()
-findDocumentsThree()
-
-
-def printLineNumbers():
-    """Make a new File with the Information."""
+def makeNewTwo():
+    """Split Nr. 2."""
     counter = 0
+    subcorpusend = '</subcorpus>'
 
-    with open('bc.processed.csv', 'r') as readfile,\
-            open('linesubcorpusinfo.txt', 'w') as writefile:
-        for line in readfile:
+    with open('bc.processed2.csv', 'r') as openfiletwo,\
+            open('bc.processed2-1.csv', 'w') as fileone,\
+            open('bc.processed2-2.csv', 'w') as filetwo,\
+            open('bc.processed2-3.csv', 'w') as filethree,\
+            open('bc.processed2-4.csv', 'w') as filefour:
+
+        for line in openfiletwo:
             counter += 1
-            if re.match('^<subcorpus', line):
-                writefile.write(str(counter) + '\t' + line + '\n')
-                # print(counter, line)
-
-        divided4 = counter / 4
-        divided3 = counter / 3
-        divided2 = counter / 2
-        writefile.write('\n' + 'No. of Lines: ' + str(counter) + '\n')
-        writefile.write('Divided by 4: ' + str(divided4) + '\n')
-        writefile.write('Divided by 3: ' + str(divided3) + '\n')
-        writefile.write('Divided by 2: ' + str(divided2) + '\n')
-        writefile.write('---------------------' + '\n')
-        writefile.write('No. 1: ' + '1' + '\n')
-        writefile.write('No. 2: ' + str(divided4) + '\t' + str(divided3) +
-                        '\t' + str(divided2) + '\n')
-        writefile.write('No. 3: ' + str(divided4 * 2) + '\t' +
-                        str(divided3 * 2) + '\n')
-        writefile.write('No. 4: ' + str(divided4 * 3) + '\n')
-
-        print('No. of Lines:', str(counter))
-        print('Divided by 4:', str(divided4))
-        print('-------------------------')
-        print('No. 1: ', '1')
-        print('No. 2: ', str(divided4), str(divided3), str(divided2))
-        print('No. 3: ', str(divided4 * 2), str(divided3 * 2))
-        print('No. 4: ', str(divided4 * 3))
-
-
-def makeNewFiles():
-    """Make four new Files."""
-    counter = 0
-
-    with open('bc.processed.csv', 'r') as readfile,\
-            open('bc.processed1.csv', 'w') as fileone,\
-            open('bc.processed2.csv', 'w') as filetwo,\
-            open('bc.processed3.csv', 'w') as filethree,\
-            open('bc.processed4.csv', 'w') as filefour:
-
-        for line in readfile:
-            counter += 1
-            if 1 <= counter <= 2404258:
+            if counter == 1:
                 fileone.write(line)
-            elif 2404259 <= counter <= 29082756:
                 filetwo.write(line)
-            elif 29082757 <= counter <= 40616955:
                 filethree.write(line)
-            elif 40616956 <= counter <= 47848267:
                 filefour.write(line)
+            elif 2 <= counter <= 6663153:
+                fileone.write(line)
+            elif 6663154 <= counter <= 13394161:
+                filetwo.write(line)
+            elif 13394162 <= counter <= 20008475:
+                filethree.write(line)
+            elif 20008476 <= counter <= 26678497:
+                filefour.write(line)
+            elif counter == 26678498:
+                print('Last Line reached Two: ', counter)
             else:
-                print('ERROR: ', counter)
+                print('ERROR Two: ', counter)
+
+        fileone.write(subcorpusend)
+        filetwo.write(subcorpusend)
+        filethree.write(subcorpusend)
+        filefour.write(subcorpusend)
+
+
+def makeNewThree():
+    """Split Nr. 3."""
+    counter = 0
+    subcorpusend = '</subcorpus>'
+
+    with open('bc.processed3.csv', 'r') as openfiletwo,\
+            open('bc.processed3-1.csv', 'w') as fileone,\
+            open('bc.processed3-2.csv', 'w') as filetwo:
+
+        for line in openfiletwo:
+            counter += 1
+            if counter == 1:
+                fileone.write(line)
+                filetwo.write(line)
+            elif 2 <= counter <= 5767659:
+                fileone.write(line)
+            elif 5767660 <= counter <= 11534198:
+                filetwo.write(line)
+            elif counter == 11534199:
+                print('Last Line reached Three: ', counter)
+            else:
+                print('ERROR Three: ', counter)
+
+        fileone.write(subcorpusend)
+        filetwo.write(subcorpusend)
+
+
+makeNewTwo()
+makeNewThree()
